e7ad28852fc6252604784ab1db26af5b9a27cd2a
ken imade <kenneth.imade@yahoo.com>
2024-03-31 18:22:52 +0100

    added highlighter and moved position of END statement

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,11 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { marked } from 'marked';
+import hljs from 'highlight.js';
 import DOMPurify from 'dompurify';
-// import PageTitleContext from '../PageTitleContext';
 import formatDate from '../hooks/dateFormat';
 
 const Post = () => {
-  // const [, setTitle] = useContext(PageTitleContext);
   const [postContent, setPostContent] = useState('');
   const [frontMatter, setFrontMatter] = useState({});
   const { slug } = useParams();
@@ -15,7 +14,6 @@ const Post = () => {
     fetch(`/posts/published/${slug}.md`)
       .then(response => response.text())
       .then(text => {
-        // Simple front matter parsing
         const splitContent = text.split('---');
         const frontMatterContent = splitContent[1];
         const markdownContent = splitContent[2];
@@ -27,25 +25,31 @@ const Post = () => {
           return acc;
         }, {});
 
+        // Configure marked with highlight.js for code syntax highlighting
+        marked.setOptions({
+          highlight: function(code, lang) {
+            const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+            return hljs.highlight(code, { language }).value;
+          }
+        });
+
         const html = marked.parse(markdownContent);
         const sanitizedHtml = DOMPurify.sanitize(html, {
-          ADD_TAGS: ['iframe']
+          ADD_TAGS: ['iframe', 'pre', 'code'],
+          ADD_ATTR: ['class'],
         });
 
         setPostContent(sanitizedHtml);
         setFrontMatter(frontMatterData);
-        // setTitle(frontMatterData.title);
-      })
+      }).then(() => {
+      hljs.highlightAll(); // Manually trigger highlighting after content is loaded
+    })
       .catch(err => console.error("Fetch error: ", err));
   }, [slug]);
-
-  // console.log(postContent)
-  // console.log(frontMatter)
 
   return (
     <>
       <article>
-        
         <header className='mb-6'>
           <h1 className="text-4xl italic font-bold">{frontMatter.title}</h1>
           <p className='text-lg italic text-blue-950 opacity-90 mt-2 p-0'>{formatDate(frontMatter.date)}</p>
@@ -55,14 +59,11 @@ const Post = () => {
           <div className="prose text-lg text-blue-950 divide-y divide-neutral-200 xl:pb-0 xl:col-span-3 xl:row-span-2" dangerouslySetInnerHTML={{ __html: postContent }} />
         </div>
 
-        <p className='text-2xl text-center'>END</p>
-
-        <p className='mt-6'><Link  to='/'>← Return to Homepage</Link></p>
+        <p className='text-2xl'>END</p>
+        <p className='mt-6'><Link to='/'>← Return to Homepage</Link></p>
         <p className='mt-4'><Link to='/posts'>Checkout my other writeups →</Link></p>
       </article>
-      
     </>
-    
   );
 };
 
